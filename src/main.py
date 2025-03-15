9296dad173695ca03c9eca034581528f42acaa03
joao9856 <joao-mm-pereira@hotmail.com>
2025-03-15 17:30:15 +0000

    added ability to copy from static to public withg predelition of public before copy, added public/ to gitignore

--- src/main.py
+++ src/main.py
@@ -1,10 +1,33 @@
 from textnode import TextNode, TextType, split_nodes_delimiter, split_nodes_image, split_nodes_link, text_to_textnodes
+import os, shutil
 
+
+def static_to_public():
+    public_path = os.path.join(os.getcwd(), "public")
+    if os.path.exists(public_path):
+        shutil.rmtree(public_path)
+        os.mkdir(public_path)
+    else:
+        os.mkdir(public_path)
+    static_path = os.path.join(os.getcwd(), "static")
+    copy_to_public(static_path, public_path)
+
+
+def copy_to_public(from_path, to_path):
+    for path in os.listdir(from_path):
+        if os.path.isfile(os.path.join(from_path, path)):
+            shutil.copy(os.path.join(from_path, path), os.path.join(to_path, path))
+            continue
+        os.mkdir(os.path.join(to_path, path))
+        copy_to_public(os.path.join(from_path, path), os.path.join(to_path, path))
+
+        
 def main():
     node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
     print(node)
-    text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
-    new_nodes = text_to_textnodes(text)
-    print(new_nodes)
+    print(os.path.dirname(__file__))
+    print(type(os.path.dirname(__file__)))
+    print(os.listdir(os.getcwd()))
+    static_to_public()
 if __name__ == "__main__":
     main()
